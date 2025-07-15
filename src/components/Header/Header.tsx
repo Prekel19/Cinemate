b19693506835f132a8fc895f935d86cea28e3f82
Prekel19 <wiktornicolasbalamut@gmail.com>
2025-07-15 19:20:37 +0200

    Added gradient background, constans for colors and shadcn library

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,20 @@
+import { Link, NavLink } from "react-router";
+import "./style.scss";
+
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="header">
+      <h1>
+        <Link to="/">
+          <span>C</span>inemate
+        </Link>
+      </h1>
+      <div className="nav-menu">
+        <NavLink to="/">All</NavLink>
+        <NavLink to="/movies">Movies</NavLink>
+        <NavLink to="/series">TV Series</NavLink>
+        <NavLink to="/watchlist">Watchlist</NavLink>
+      </div>
+    </header>
+  );
 };
--- src/components/Layout/MainLayout.tsx
+++ src/components/Layout/MainLayout.tsx
@@ -4,12 +4,12 @@ import { Footer } from "../Footer/Footer";
 
 export const MainLayout = () => {
   return (
-    <>
+    <div className="app">
       <Header />
       <main>
         <Outlet />
       </main>
       <Footer />
-    </>
+    </div>
   );
 };
