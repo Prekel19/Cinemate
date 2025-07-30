79d126d350e2ede7b4db981dd22b10903ab5001c
Prekel19 <wiktornicolasbalamut@gmail.com>
2025-07-30 19:53:21 +0200

    Changed watchlist button value

--- src/components/MediaDetails/MediaDetailsButtons.tsx
+++ src/components/MediaDetails/MediaDetailsButtons.tsx
@@ -7,7 +7,7 @@ export const MediaDetailsButtons = () => {
     <div className="media-details-buttons">
       <Button className="media-details-button watchlist">
         <Bookmark size={24} />
-        Add to watch list
+        Add to Watchlist
       </Button>
       <Button className="media-details-button share">
         <Share size={24} /> Share
